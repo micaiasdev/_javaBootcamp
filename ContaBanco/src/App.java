2b7b164f9d8363234c044220a906f628f740ec2e
micaias <miciaiasvieira@gmail.com>
2024-06-19 18:41:27 -0300

    add_archives

---
+++ ContaBanco/src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       @SuppressWarnings("resource")
+        Scanner teclado = new Scanner(System.in);
+       ContaTerminal account = new ContaTerminal();
+       
+       System.out.println("Digite o número da Conta:");
+       account.setNumero(teclado.nextInt());
+
+       System.out.println("Digite o número da Agência:");
+       account.setAgencia(teclado.next());
+       
+       System.out.println("Digite Seu nome:");
+       account.setNomeCliente(teclado.next());
+       teclado.nextLine();
+
+       System.out.println("Digite Seu Saldo:");
+       account.setSaldo(Double.parseDouble(teclado.next()));
+       
+       account.printfeedbackCreateAccount();
+       
+    }
+}
